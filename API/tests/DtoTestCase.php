36142939bd113b83c57ff74251ffbf4f77fff69f
Eugene L <euglihon.dev@gmail.com>
2023-11-04 19:56:05 +0100

    the test logic for dto has been created

---
+++ API/tests/DtoTestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class DtoTestCase extends TestCase
+{
+    protected ValidatorInterface $validator;
+
+    protected function setUp(): void
+    {
+        $this->validator = Validation::createValidatorBuilder()
+            ->enableAnnotationMapping()
+            ->getValidator();
+    }
+
+    protected function inspect($violations): array
+    {
+        $errorMessages = [];
+        if (count($violations) > 0) {
+            foreach ($violations as $error) {
+                $field = $error->getPropertyPath();
+                $message = $error->getMessage();
+                $errorMessages[$field] = $message;
+            }
+        }
+        return $errorMessages;
+    }
+}
